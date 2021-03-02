580e199c2525bce11e26752e3c9a998942ab5f3b
RenanIOliveira <renanioliveira@dcc.ufrj.br>
2021-03-01 23:11:03 -0300

    Fixing logs and names in prog1.c

--- prog1.c
+++ prog1.c
@@ -11,15 +11,15 @@ int main(void)
     int status, id, j;
     int pid = getpid();
 
-    //pipe to send messages from father to child
+    //pipe to send messages from parent to child
     int size_message_to_child;
-    int pipe_father_to_child[2];
-    pipe(pipe_father_to_child);
+    int pipe_parent_to_child[2];
+    pipe(pipe_parent_to_child);
 
-    //pipe to send messages from child to father
-    int size_message_to_father;
-    int pipe_child_to_father[2];
-    pipe(pipe_child_to_father);
+    //pipe to send messages from child to parent
+    int size_message_to_parent;
+    int pipe_child_to_parent[2];
+    pipe(pipe_child_to_parent);
 
     char buffer[1025];
 
@@ -27,44 +27,44 @@ int main(void)
 
     int pid2 = fork();
     if (pid2 != 0) {
-        //FATHER
+        //PARENT
         int pid_filho = pid2;
-        printf("FATHER: pid: %d \n", getpid());
+        printf("PARENT: pid: %d \n", getpid());
         
         char *message = "Luke, I am your father.";
-        printf("FATHER: Sending Message: %s\n",message);
+        printf("PARENT: Sending Message: %s\n",message);
        
         //Using pipe to send the message
-        write(pipe_father_to_child[1], message, strlen(message));
-        close(pipe_father_to_child[1]);
+        write(pipe_parent_to_child[1], message, strlen(message));
+        close(pipe_parent_to_child[1]);
 
         
-        printf("FATHER: waiting message from child...\n");
-        //wait for message from father
-        if ((size_message_to_father = read ( pipe_child_to_father[0], buffer, MSG_SIZE ) ) >= 0) {
-            buffer[size_message_to_father] = 0;  //terminate the string
-            printf("FATHER: Received Message: %s \n",  buffer);
+        printf("PARENT: waiting message from child...\n");
+        //wait for message from parent
+        if ((size_message_to_parent = read ( pipe_child_to_parent[0], buffer, MSG_SIZE ) ) >= 0) {
+            buffer[size_message_to_parent] = 0;  //terminate the string
+            printf("PARENT: Received Message: %s \n",  buffer);
         }else{
-            printf("FATHER: Error reading from pipe\n");
+            printf("PARENT: Error reading from pipe\n");
         }
 
         
-        printf("FATHER: waiting child to end...\n");
+        printf("PARENT: waiting child to end...\n");
         waitpid(pid_filho, &status, 0);
 
-        printf("FATHER: Child ended. Ending father too. \n");
+        printf("PARENT: Child ended. Ending parent too. \n");
         exit(0);
 
     } else {
         //CHILD
         int pid_filho = getpid();
         printf("CHILD: pid: %d \n",pid_filho);
-        printf("CHILD: pid do pai: %d\n", pid);
+        printf("CHILD: parent's pid: %d\n", pid);
 
 
-        printf("CHILD: waiting message from father...\n");
-        //wait for message from father
-        if ((size_message_to_child = read ( pipe_father_to_child[0], buffer, MSG_SIZE ) ) >= 0) {
+        printf("CHILD: waiting message from parent...\n");
+        //wait for message from parent
+        if ((size_message_to_child = read ( pipe_parent_to_child[0], buffer, MSG_SIZE ) ) >= 0) {
             buffer[size_message_to_child] = 0;  //terminate the string
             printf("CHILD: Received Message: %s \n",  buffer);
         }else{
@@ -75,8 +75,8 @@ int main(void)
         printf("CHILD: Sending Message: %s\n",message);
        
         //Using pipe to send the message
-        write(pipe_child_to_father[1], message, strlen(message));
-        close(pipe_child_to_father[1]);
+        write(pipe_child_to_parent[1], message, strlen(message));
+        close(pipe_child_to_parent[1]);
 
         int j;
         for (j = 0; j <= 10000; j++);
@@ -88,8 +88,8 @@ int main(void)
         printf("CHILD: Sending new message: %s\n",str_j);
             
         //Using pipe to send the message
-        write(pipe_child_to_father[1], str_j, strlen(message));
-        close(pipe_child_to_father[1]);
+        write(pipe_child_to_parent[1], str_j, strlen(message));
+        close(pipe_child_to_parent[1]);
 
         execl("/bin/ls", "ls", (char*)0);
 
